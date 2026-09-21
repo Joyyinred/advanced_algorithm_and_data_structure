ea68c9753de3b51eb6fcdf4066adb272cba295dc
Guxin Zhang <gz222bd@student.lnu.se>
2026-09-21 14:16:51 +0200

    finish empty and len

--- a2_task2.py
+++ a2_task2.py
@@ -4,7 +4,7 @@
 
 class LinkedList ():
     def __init__(self):
-        self.lst = None
+        self.lst = None  # the head of linked list
     def append(self , d):
         if self.lst is None:
             self.lst = LLNode(d)
@@ -13,6 +13,29 @@ class LinkedList ():
             while p.nxt is not None:
                 p = p.nxt
             p.nxt = LLNode(d)
+
+    # empty
+    def empty(self):
+        if self.lst is None:
+            return True
+        else:
+            return False
+
+    # another way for empty:
+    # return self.lst is None
+
+    # len
+    def __len__(self):
+        count = 0
+        p = self.lst
+        while p is not None:
+            count += 1
+            p = p.nxt
+        return count
+            
+
+
+
 
 class LLNode:
     def __init__(self , d):
