65daca4d593bb46c2224a006dfe9a764cd7a08dc
Guxin Zhang <gz222bd@student.lnu.se>
2026-09-21 14:48:40 +0200

    task2: finish delete

--- a2_task2.py
+++ a2_task2.py
@@ -32,6 +32,24 @@ class LinkedList ():
             count += 1
             p = p.nxt
         return count
+
+    # delete
+    # Deletes the first node with value d (same behavior as SimpleList.delete, slide 24).
+    # If d is not in the list, the list is unchanged.
+    def delete(self, d):
+        if self.lst is None:
+            return
+        elif self.lst.val == d:
+            self.lst = self.lst.nxt
+        else:
+            p = self.lst
+            while p.nxt is not None:
+                if p.nxt.val == d:
+                    p.nxt = p.nxt.nxt  # skip the node we wanna delete
+                    return  # after delete the value, end the function in case it crash,only delete the first node that has the value
+                
+                p = p.nxt
+                     
             
 
 
